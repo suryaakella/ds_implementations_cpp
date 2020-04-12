52fb3b32f98abbc59ef2a10a9fb4cf0564765f40
suryaakella <47234885+suryaakella@users.noreply.github.com>
2020-04-12 14:45:29 +0530

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class Node{
@@ -33,6 +37,65 @@ class bst{
 			preorder(temp->left); 
 			preorder(temp->right);}
 	}
+	
+	void iterativepreorder(Node* temp){
+		stack <Node*> s;
+		Node* temp2 = temp;
+		while(!s.empty() || temp2){
+			if(temp2){
+				cout<<temp2->data<<" ";
+				s.push(temp2);
+				temp2 = temp2->left;
+				}
+			else{	temp2 = s.top();
+					s.pop();
+					temp2 = temp2->right;
+				}
+			}
+		}
+	void iterativeinorder(Node* temp){
+		stack <Node*> s;
+		Node* temp2 = temp;
+		while(!s.empty() || temp2){
+			if(temp2){
+				s.push(temp2);
+				temp2 = temp2->left;
+				}
+			else{			
+					temp2 = s.top();
+					s.pop();
+					cout<<temp2->data<<" ";
+					temp2 = temp2->right;
+				}
+			}
+		}
+
+	void iterativepostorder(Node* temp){
+		stack <Node*> s;
+		stack <int> s2;
+		Node* temp2 = temp;
+		while(!s.empty() || temp2){
+			if(temp2){
+				cout<<temp2->data<<" ";
+				s.push(temp2);
+				s2.push(temp2->data);
+				temp2 = temp2->right;
+				}
+			else{	temp2 = s.top();
+					s.pop();
+					temp2 = temp2->left;
+				}
+			}
+			cout<<endl;
+		while(!s2.empty()){cout<<s2.top()<<" "; s2.pop();}
+		}
+
+	int totaldepth(Node* temp){
+		if(!temp) return 0;
+		
+		return (1+std::max(totaldepth(temp->left),totaldepth(temp->right))); 
+		}
+
 	//LNR
 	void inorder(Node* temp){
 		if(temp){
@@ -47,7 +110,163 @@ class bst{
 			postorder(temp->right);
 			cout<<temp->data<<" ";
 			}
-	}	
+	}
+	
+	vector<vector<int>> levelorder(Node* temp){
+		queue <Node*> q;
+		int count;
+		vector<vector<int>> result;
+		int total_depth = totaldepth(temp);
+		vector<int> *vec;
+		int level_count = 0;
+		vec = new vector <int> [total_depth+1];
+		q.push(temp);
+		while(!q.empty()){
+			count = q.size();
+			level_count++;
+			while(count>0){
+				temp = q.front();
+				cout<<temp->data<<" ";
+				vec[level_count].push_back(temp->data);
+				q.pop();
+				if(temp->left) {q.push(temp->left); }
+				if(temp->right) {q.push(temp->right);}
+				count--;
+				}
+				cout<<endl;
+				}
+		//for(int i = 1 ; i <=total_depth; i++){ for(auto it:vec[i]) cout<<it<<" "; cout<<endl;}		
+		for(int i = 1 ; i<=total_depth; i++){ result.push_back(vec[i]); }
+		//cout<<result[0][0]<<"-----";
+		return result;
+	}
+	
+	void leftview(Node* temp){
+		vector<vector<int>> result = levelorder(temp);
+		int total_depth = totaldepth(temp);
+		for(int i = 0; i < total_depth; i++){cout<<result[i][0]<<" ";}
+		cout<<endl;
+		}
+
+	void rightview(Node* temp){
+		vector<vector<int>> result = levelorder(temp);
+		int total_depth = totaldepth(temp);
+		for(int i = 0; i < total_depth; i++){cout<<result[i].back()<<" ";}
+		cout<<endl;
+		}
+		
+	unordered_map <int, vector<int>> u;
+	int min_negative_for_vertical_order=0, max_positive_for_vertical_order=0;
+	void preorderforverticalorder(Node* temp, int len){
+		if(temp){
+			u[len].push_back(temp->data); 
+			if(min_negative_for_vertical_order > len) min_negative_for_vertical_order = len;
+			if(max_positive_for_vertical_order < len) max_positive_for_vertical_order = len;			
+			preorderforverticalorder(temp->left, len-1);
+			preorderforverticalorder(temp->right, len+1);
+			}
+		}
+				
+	void verticalorder(Node* temp){
+		u.clear();
+		preorderforverticalorder(temp, 0);
+	//	cout<<min_negative_for_vertical_order<<"-----"<<max_positive_for_vertical_order<<endl;
+		for(int i = min_negative_for_vertical_order; i<= max_positive_for_vertical_order; i++){
+			for(auto it:u[i]) cout<<it<<" "; 
+			cout<<endl;
+			}
+		u.clear();
+		}
+		
+	void topview(Node* temp){
+		u.clear();
+		preorderforverticalorder(temp, 0);
+	//	cout<<min_negative_for_vertical_order<<"-----"<<max_positive_for_vertical_order<<endl;
+		for(int i = min_negative_for_vertical_order; i<= max_positive_for_vertical_order; i++){
+			cout<<u[i][0]<<" "; 
+			}
+		u.clear();
+		}
+		
+	void bottomview(Node* temp){
+		u.clear();
+		preorderforverticalorder(temp, 0);
+	//	cout<<min_negative_for_vertical_order<<"-----"<<max_positive_for_vertical_order<<endl;
+		for(int i = min_negative_for_vertical_order; i<= max_positive_for_vertical_order; i++){
+			cout<<u[i].back()<<" "; 
+			}
+		u.clear();	
+		}
+			
+	void preorderfordiagonalorder(Node* temp, int len){
+		if(temp){
+		 	u[len].push_back(temp->data); 
+		 	if(min_negative_for_vertical_order > len) min_negative_for_vertical_order = len;
+			if(max_positive_for_vertical_order < len) max_positive_for_vertical_order = len;	
+			preorderfordiagonalorder(temp->left, len+1);
+			preorderfordiagonalorder(temp->right, len);
+			}
+		}
+			
+	void diagonaltraversal(Node* temp){
+		u.clear();
+		preorderfordiagonalorder(temp, 0);
+		for(int i = 0; i<= max_positive_for_vertical_order; i++){
+			for(auto it:u[i]) cout<<it<<" "; 
+			cout<<endl;
+			}
+		u.clear();
+		}
+	void leftviewwithoutleaf(Node* temp){
+		while(temp->left){cout<<temp->data<<" "; temp = temp->left;}
+		}
+	void printleafnodes(Node* temp){if(temp->left == NULL && temp->right == NULL) cout<<temp->data<<" ";
+		else{
+			printleafnodes(temp->left);
+			printleafnodes(temp->right);
+			}
+		}
+	void rightviewwithoutleaf(Node* temp){
+		stack <int> s;
+		while(temp->right){s.push(temp->data); temp = temp->right;}
+		while(!s.empty()){if(s.size() == 1) break; else cout<<s.top()<<" "; s.pop();}
+		}
+		
+	void boundarytraversal(Node* temp){
+		leftviewwithoutleaf(temp);
+		printleafnodes(temp);
+		rightviewwithoutleaf(temp);
+		}
+	void bsttomirrortree(Node* temp){
+		if(temp){
+			bsttomirrortree(temp->left);
+			bsttomirrortree(temp->right);
+			swap(temp->left, temp->right);
+			}
+		}
+		
+	void spiralorder(Node* temp){
+		stack <Node*> s1,s2; 
+		s1.push(temp);
+		while(!s1.empty() || !s2.empty()){
+			while(!s1.empty()){
+				temp = s1.top();
+				cout<<temp->data<<" ";
+				s1.pop();
+				if(temp->left) s2.push(temp->left);
+				if(temp->right) s2.push(temp->right);			
+				}
+				
+			while(!s2.empty()){
+				temp = s2.top();
+				cout<<temp->data<<" ";
+				s2.pop();
+				if(temp->right) s1.push(temp->right);							
+				if(temp->left) s1.push(temp->left);				
+				}
+			}
+		}
+	
 	Node* min(Node* temp){while(temp->left){temp = temp->left;} return temp;} //minimum at temp->left
 	Node* max(Node* temp){while(temp->right){temp = temp->right;} return temp;} //maximum at temp->right
 	
@@ -58,7 +277,7 @@ class bst{
 	else {
 		if(root->left == NULL && root->right == NULL) { 
 			delete root;
-			root = NULL;
+			root = NULL; //chill scene
 		}
 		else if(root->left == NULL) {
 			Node *temp = root;
@@ -77,7 +296,7 @@ class bst{
 		}
 	}
 	return root;
-}
+} 
 	};
 
 int main(){
@@ -91,11 +310,37 @@ int main(){
 	p->insert(10);
 	p->insert(2);	
 	p->insert(4);
-	p->preorder(p->root);
+//	p->preorder(p->root);
 	cout<<endl;
-	p->del(p->root, 3);
+//	p->del(p->root, 3);
 	cout<<endl;
-	p->preorder(p->root);
+//	p->postorder(p->root);
 	cout<<endl;
+//	p->iterativepostorder(p->root);
+	cout<<endl;
+	//p->levelorder(p->root);
+	cout<<endl;
+//	cout<<p->totaldepth(p->root);
+	cout<<endl;
+//	p->levelorder(p->root);
+	cout<<endl;
+//	p->leftview(p->root);
+	cout<<endl;
+//	p->rightview(p->root);
+//	p->verticalorder(p->root);
+//	cout<<endl;
+//	p->topview(p->root);
+//	cout<<endl;
+//	p->bottomview(p->root);
+//	p->diagonaltraversal(p->root);
+//	cout<<endl;
+//	p->boundarytraversal(p->root);
+//	cout<<endl;
+//	p->bsttomirrortree(p->root);
+//	cout<<endl;
+//	p->preorder(p->root);
+//	cout<<endl;
+//	p->spiralorder(p->root);
+//	cout<<endl;
 	return 0;
 	}
